faf3db7466b902a0a6bc7d2bdfa06087da80cb12
Tobias <tobias_maier99@gmx.de>
2016-09-13 16:07:10 +0200

    jsonify ip

--- flask_app.py
+++ flask_app.py
@@ -2,10 +2,12 @@
 flask app (web server)
 """
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return str(request.remote_addr)
+    """ simply returns the IP of the caller """
+    return jsonify({"ip": request.environ.get('HTTP_X_REAL_IP',
+                                              request.remote_addr)}), 200
